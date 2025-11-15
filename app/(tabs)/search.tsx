7a18a163788d8aa130ca8cd45d22c53cdd75e158
Rahat <rahatminhaj555@gmail.com>
2025-11-16 00:15:32 +0900

    basic tab router setup done

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Search = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl text-primary font-bold">Search!!</Text>
+        </View>
+    );
+}
+
+export default Search;
+
+
---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const MovieDetails = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl text-primary font-bold">MovieDetails!!</Text>
+        </View>
+    );
+}
+
+export default MovieDetails;
+
+
